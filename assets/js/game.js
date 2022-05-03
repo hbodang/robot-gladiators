14af294607e130f11be74f6aedf15ead36f5865b
hbodang <haleylyndennis@gmail.com>
2022-05-02 21:12:07 -0400

    created initial logic of fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,39 @@
 var playerName = window.prompt("What's your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(10+10);
-
-console.log("Our robot's name is ", playerName);
-
-function fight()
+var fight = function()
 {
-    window.alert("The fight has begun!");
-}
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName, " attacked ", enemyName, ". ", enemyName, " now has ", enemyHealth, " health remaining.");
 
-// fight();
+    if (enemyHealth <= 0)
+    {
+        window.alert(enemyName + " has died!");
+    }
+    else
+    {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
 
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName, " attacked ", playerName, ". ", playerName, " now has ", playerHealth, " health remaining.");
+
+    if (playerHealth <= 0)
+    {
+        window.alert(playerName + " has died!");
+    }
+    else
+    {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+};
+
+fight();
